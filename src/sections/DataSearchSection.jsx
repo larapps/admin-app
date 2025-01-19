5c7b14d17bfc565e969dfa2cd56dd305c87d86f6
larapps <149411474+larapps@users.noreply.github.com>
2025-01-19 18:20:14 +0530

    Column Updates

--- src/sections/DataSearchSection.jsx
+++ src/sections/DataSearchSection.jsx
@@ -12,7 +12,154 @@ import Pagination from "react-js-pagination";
 
 function DataSearchSection(){
 
-    const columns = [{ "name": "employeeId", "display": "Emp Id", "fixedClass": "column-1" }, { "name": "currentWorkingLocation", "display": "Curr Wrk Loc", "fixedClass": "column-2" }, { "name": "workType", "display": "Wrk Type" }, { "name": "empType", "display": "Emp Type" }, { "name": "month", "display": "Month" }, { "name": "contractNumber", "display": "Contract Num" }, { "name": "designation", "display": "Designation" }, { "name": "totalNormalDays", "display": "Total Normal Days" }, { "name": "totalWeeklyOffDays", "display": "Total WklyOff Days" }, { "name": "earnedBasic", "display": "Earned Basic" }, { "name": "earnedSitebasic", "display": "Earned Site Basic" }, { "name": "earnedExtraot", "display": "Earned Extra OT" }, { "name": "earnedNormalovertime", "display": "Earned Normal OT" }, { "name": "earnedNormalovertimeot68", "display": "Earned Normal OT68" }, { "name": "earnedPublicholidayot", "display": "Earned Pub Holiday OT" }, { "name": "earnedRamadanot", "display": "Earned Ramadan OT" }, { "name": "earnedRamadanot68", "display": "Earned Ramadan OT68" }, { "name": "earnedWeeklyoffot", "display": "Earned Wkly Off OT" }, { "name": "securityAllowance", "display": "SA" }, { "name": "rolebasedAllowance", "display": "RBA" }, { "name": "sitebasedAllowance", "display": "SBA" }, { "name": "foodAllowance", "display": "FA" }, { "name": "houserentalAllowance", "display": "HRA" }, { "name": "transportAllowance", "display": "TA" }, { "name": "vehicleAllowance", "display": "VA" }, { "name": "otherAllowance", "display": "OA" }, { "name": "weekoffAllowance", "display": "Wk off Allowance" }, { "name": "benefits", "display": "Benefits" }, { "name": "serviceBenefits", "display": "Service Benefits" }, { "name": "training", "display": "Training" }, { "name": "eosb", "display": "EOSB" }, { "name": "leaveSalary", "display": "Leave Salary" }, { "name": "medicine", "display": "Medicine" }, { "name": "travel", "display": "Travel" }, { "name": "transport", "display": "Transport" }, { "name": "vehicleRental", "display": "Vehicle Rental" }, { "name": "visa", "display": "Visa" }, { "name": "accommodation", "display": "Accommodation" }, { "name": "workmen", "display": "Workmen" }, { "name": "idUserCreated", "display": "User Created" }, { "name": "idUserModified", "display": "User Modified" }, { "name": "totalDeduction", "display": "Total Deduction" }, { "name": "totalEarnings", "display": "Total Earnings" } ];
+    const columns = [
+        {
+          "name": "contractNumber",
+          "display": "Contract Num",
+          "fixedClass": "column-1"
+        },
+        {
+          "name": "currentWorkingLocation",
+          "display": "CWL",
+            "fixedClass": "column-2"
+        },
+        {
+          "name": "employeeId",
+          "display": "Emp Id"
+        },
+        {
+          "name": "designation",
+          "display": "Designation"
+        },
+        {
+          "name": "workType",
+          "display": "Wrk Type"
+        },
+        {
+          "name": "totalNormalDays",
+          "display": "Total Normal Days"
+        },
+        {
+          "name": "totalWeeklyOffDays",
+          "display": "Total WklyOff Days"
+        },
+        {
+          "name": "earnedBasic",
+          "display": "Earned Basic"
+        },
+        {
+          "name": "earnedSitebasic",
+          "display": "Earned Site Basic"
+        },
+        {
+          "name": "earnedNormalovertime",
+          "display": "Earned Normal OT"
+        },
+        {
+          "name": "earnedNormalovertimeot68",
+          "display": "Earned Normal OT68"
+        },
+        {
+          "name": "earnedExtraot",
+          "display": "Earned Extra OT"
+        },
+        {
+          "name": "earnedRamadanot",
+          "display": "Earned Ramadan OT"
+        },
+        {
+          "name": "earnedRamadanot68",
+          "display": "Earned Ramadan OT68"
+        },
+        {
+          "name": "earnedWeeklyoffot",
+          "display": "Earned Wkly Off OT"
+        },
+        {
+          "name": "earnedPublicholidayot",
+          "display": "Earned Pub Holiday OT"
+        },
+        {
+          "name": "securityAllowance",
+          "display": "SA"
+        },
+        {
+          "name": "rolebasedAllowance",
+          "display": "RBA"
+        },
+        {
+          "name": "sitebasedAllowance",
+          "display": "SBA"
+        },
+        {
+          "name": "foodAllowance",
+          "display": "FA"
+        },
+        {
+          "name": "houserentalAllowance",
+          "display": "HRA"
+        },
+        {
+          "name": "transportAllowance",
+          "display": "TA"
+        },
+        {
+          "name": "vehicleAllowance",
+          "display": "VA"
+        },
+        {
+          "name": "otherAllowance",
+          "display": "OA"
+        },
+        {
+          "name": "serviceBenefits",
+          "display": "Service Benefits"
+        },
+        {
+          "name": "benefits",
+          "display": "Benefits"
+        },
+        {
+          "name": "leaveSalary",
+          "display": "Leave Salary"
+        },
+        {
+          "name": "eosb",
+          "display": "EOSB"
+        },
+        {
+          "name": "medicine",
+          "display": "Medicine"
+        },
+        {
+          "name": "workmen",
+          "display": "Workmen"
+        },
+        {
+          "name": "vehicleRental",
+          "display": "Veh Rent"
+        },
+        {
+          "name": "training",
+          "display": "Training"
+        },
+        {
+          "name": "visa",
+          "display": "Visa"
+        },
+        {
+          "name": "transport",
+          "display": "Transport"
+        },
+        {
+          "name": "travel",
+          "display": "Travel"
+        },
+        {
+          "name": "accommodation",
+          "display": "Accommodation"
+        }
+      ];
     const [rows, setRows] = useState([]);
     const [showTable, setShowTable] = useState(false);
 
